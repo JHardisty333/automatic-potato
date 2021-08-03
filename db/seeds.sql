232b1a221aa9b62a9c9026562a7cd54e17591c12
unknown <jtjackson325@gmail.com>
2021-08-03 15:28:19 -0600

    I got the seeds part of the db folder set up and running for our database

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO calendar (type, book, page, date, time)
+VALUES 
+    ('book report', 'The Great Gatsby', NULL, '2022-6-13', '11:59:00'),
+    ('reading summary', 'Lord of The Flies', 198, '2022-6-21', '11:59:00'),
+    ('grammar worksheet', NULL, NULL, '2022-6-21', '11:59:00'),
+    ('reading journal', 'Off The Record', NULL, '2022-7-02', '11:59:00');
+
+INSERT INTO newsfeed (name, message)
+VALUES 
+    ('Mrs. Jenkins', 'Remeber to turn in your assignments in on time to avoid the ten percent penalty if your homework is late'),
+    ('Mrs. Jenkins', 'The grammar test we were going to have on Wednesday has now been moves to Tuesday next week'),
+    ('Mrs. Jenkins', 'The list of approved books for the book report now includes Percy Jackson: The Titans Curse and Grey Grifins: The Brimstone Key'),
+    ('Mrs. Jenkins', 'There will be a sub for the next five days of class. My sister just had a baby and needs my help taking care of it');
+
+INSERT INTO contact (name, email)
+VALUES 
+    ('Mrs. Jenkins', 'Mjjenkins@byu.edu'),
+    ('Sabrina', 'sabthebomb24#@gmail.com'),
+    ('Joseph', 'jman1pro@msn.com'),
+    ('Bruce', 'batmanwayne34#@gmail.com');
+
+INSERT INTO bell_ringer (name, story)
+VALUES 
+    ('Mrs. Jenkins', 'I had a pet penguin when I was a little girl'),
+    ('Joey', 'I have been to nine different countries in Europe'),
+    ('Stephanie', 'I have an extra toe on my big foot'),
+    ('Hudson', 'I have run in three iron man marathons');
