14adde5727e930a06e2796b40166d903d474ccb8
Mikeysoar <mwclimb@gmail.com>
2021-08-03 20:29:55 -0600

    following 14 20210803-2029

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/api'));
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
