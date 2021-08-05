e879a466b22a71766ba40cfd6cfa9cf3d62cb173
unknown <jtjackson325@gmail.com>
2021-08-05 10:10:50 -0600

    got the majority of our new backend done based off mdoule 13

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
+   secret: 'Super secret secret',
+   cookie: {},
+   resave: false,
+   saveUninitialized: true,
+   store: new SequelizeStore({
     db: sequelize
   })
 };
@@ -22,7 +22,6 @@ const sess = {
 app.use(session(sess));
 
 const hbs = exphbs.create({});
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -30,9 +29,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(require('./controllers/api'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+   app.listen(PORT, () => console.log('Now listening'));
 });
