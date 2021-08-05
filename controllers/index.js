754a2e1082be6c3fa41591f4c87e1ca46dde9541
Mikeysoar <mwclimb@gmail.com>
2021-08-05 11:33:00 -0600

    fixing merge conflicts

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
 // const studentRoutes = require('./student-routes.js');
-const teacherRoutes = require('./teacher-routes.js');
+// const teacherRoutes = require('./teacher-routes.js');
 
 
 // router.use('/student', studentRoutes);
-router.use('/', teacherRoutes);
+// router.use('/', teacherRoutes);
 
-module.exports = router;
+// module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,4 @@
+const router = require('express').Router();
 
 const newsfeedRoutes = require('./newsfeed-routes.js');
 const userRoutes = require('./user-routes.js');
