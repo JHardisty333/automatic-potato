9bceeeaa97edba06422b7787868aa8392576e0f5
unknown <jtjackson325@gmail.com>
2021-08-06 12:06:07 -0600

    started creating the routes for the last database

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const newsfeedRoutes = require('./newsfeed-routes.js');
 const userRoutes = require('./user-routes.js');
+const newsupdateRoutes = require('./newsupdate-routes');
 
 
 router.use('/newsfeed', newsfeedRoutes);
 router.use('/users', userRoutes);
+router.use('/newsupdate', newsupdateRoutes);
 
 module.exports = router;
 
---
+++ routes/api/newsupdate-routes.js
@@ -0,0 +1,26 @@
+const router = requrie ('express').Router();
+const { Newsupdate } = require('../../models');
+
+router.get('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+    Newsupdate.create({
+        newupdate_text: req.body.newupdate_text,
+        user_id: req.body.user_id,
+        newsfeed_id: req.body.newsfeed_id
+    })
+        .then(dbNewsupdateData => res.json(dbNewsupdateData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
