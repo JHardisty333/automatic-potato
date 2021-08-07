2de6aaf07609226b059f7d7669a04fb19fdd86d2
unknown <jtjackson325@gmail.com>
2021-08-06 18:29:09 -0600

    got the project set up for the future heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
-// import the Sequlize constructor from the library 
-const Sequelize = require('sequelize');
+const { Sequelize } = require("sequelize/types");
 
-require('dotenv').config();
+let sequelize;
 
-// create a connection to our database, pass in your MYSQL information for the user
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
-
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
