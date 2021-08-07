91dcfe8c27322be2d2af26b6af9410e2c51d0c42
unknown <jtjackson325@gmail.com>
2021-08-06 18:17:13 -0600

    added all of the code for the comments to be included with the posts the students make

--- routes/api/newsupdate-routes.js
+++ routes/api/newsupdate-routes.js
@@ -2,8 +2,13 @@ const router = requrie ('express').Router();
 const { Newsupdate } = require('../../models');
 
 router.get('/', (req, res) => {
-
-});
+    Newsupdate.findAll({})
+        .then(dbNewsupdateData => res.json(dbNewsupdateData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    });
 
 router.post('/', (req, res) => {
     Newsupdate.create({
@@ -20,7 +25,22 @@ router.post('/', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+    Newsupdate.destroy({
+        where:{
+            id: req.params.id
+        }
+    })
+        .then(dbNewsupdateData => {
+            if (!dbNewsupdateData) {
+                res.status(400).json({ message: "No newsupate was found with this id!" });
+                return;
+            }
+            res.json(dbNewsupdateData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
