20f2eda77361763db3aaa910e582fa5063bfd05c
Mikeysoar <mwclimb@gmail.com>
2021-08-09 20:42:13 -0600

    added a post route and singlepost handleparts 20210809-2042

--- controllers/teacher-routes.js
+++ controllers/teacher-routes.js
@@ -1,7 +1,7 @@
 // const express = require('express');
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Newsfeed, User } = require('../models');
+const { Newsfeed, User, Newsupdate } = require('../models');
 
 
 router.get('/', (req, res) => {
@@ -44,5 +44,63 @@ router.get('/', (req, res) => {
   router.get('/', (req, res) => {
     console.log(req.session);
   })
+
+  // router.get('/post/:id', (req, res) => {
+  //   const post = {
+  //     id: 1,
+  //     newsfeed_url: 'https://google.com/',
+  //     name: 'Handlebars Docs',
+  //     created_at: new Date(),
+  //     comments: [{}, {}],
+  //     user: {
+  //       username: 'test_user'
+  //     }
+  //   };
+  
+  //   res.render('single-post', { post });
+  // });
+
+  router.get('/post/:id', (req, res) => {
+    Newsfeed.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'newsfeed_url',
+        'name'                
+      ],
+      include: [
+        {
+          model: Newsupdate,
+          attributes: ['id', 'newsupdate_text', 'newsfeed_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbNewsfeedData => {
+        if (!dbNewsfeedData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbNewsfeedData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', { post });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
