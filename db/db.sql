3e9f8cc201e8aa340ff097eeb39a1f7e21f46877
unknown <jtjackson325@gmail.com>
2021-08-14 17:36:30 -0600

    I have tried everything and will go to office hours tomorrow to connect the databases

--- db/db.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS automatic_potato;
-
-CREATE DATABASE automatic_potato;quit
--- seeds/index.js
+++ seeds/index.js
@@ -5,7 +5,7 @@ const seedNewsupdate = require('./newsupdate-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-    await sequelize.sync({ force: false });
+    await sequelize.sync({ force: true });
     console.log('----------');
     
     await seedUsers.sync();
