b881b375679dd996c92f784eb841ccdfa432955f
JHardisty333 <jessica1990_37@hotmail.com>
2021-08-22 12:50:09 -0600

    fixes again??

--- controllers/teacher-routes.js
+++ controllers/teacher-routes.js
@@ -3,59 +3,59 @@ const sequelize = require('../config/connection');
 const { Newsfeed, User, Newsupdate } = require('../models');
 
 
-// router.get('/', (req, res) => {
-//   console.log('==============');
+router.get('/', (req, res) => {
+  console.log('==============');
 
-//     Newsfeed.findAll({
-//       attributes: [
-//         'id',
-//         'newsfeed_url',
-//         'name',
-//         'message'
-//       ],
-//       include: [
-//         {
-//           model: Newsupdate,
-//           attributes: ['id', 'newsupdate_text', 'newsfeed_id', 'user_id',],
-//           include: {
-//             model: User,
-//             attributes: ['username']
-//           }
-//         },
-//         {
-//         model: User,
-//         attirbutes: ['username']
-//       }
-//     ]
-//   })
-//     .then(dbNewsfeedData => {
-//       console.log(dbNewsfeedData[0]);
-//       const posts = dbNewsfeedData.map(post => post.get({ plain: true }));
+    Newsfeed.findAll({
+      attributes: [
+        'id',
+        'newsfeed_url',
+        'name',
+        'message'
+      ],
+      include: [
+        {
+          model: Newsupdate,
+          attributes: ['id', 'newsupdate_text', 'newsfeed_id', 'user_id',],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+        model: User,
+        attirbutes: ['username']
+      }
+    ]
+  })
+    .then(dbNewsfeedData => {
+      console.log(dbNewsfeedData[0]);
+      const posts = dbNewsfeedData.map(post => post.get({ plain: true }));
 
-//       res.render('homepage', { posts });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
-//   res.render('login');
-// });
-
-// router.get('/', (req, res) => {
-//  console.log(req.session);
-// });
+  res.render('login');
+});
 
 router.get('/', (req, res) => {
-  res.render("homepage")
-})
+ console.log(req.session);
+});
+
+// router.get('/', (req, res) => {
+//   res.render("homepage")
+// })
 
 router.get('/post/:id', (req, res) => {
   Newsfeed.findOne({
